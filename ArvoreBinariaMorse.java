aacec5f5b33836e2e0fa535c91666a49e56898a1
TiagoFollador <tiagobritofollador@gmail.com>
2024-09-26 00:40:34 -0300

    inicio da arvore

---
+++ ArvoreBinariaMorse.java
@@ -0,0 +1,17 @@
+public class ArvoreBinariaMorse {
+    private Node raiz;
+
+    public ArvoreBinariaMorse() {
+        this.raiz = new Node();
+    }
+
+    public void insert(String morse, char Caracter )
+    {
+        char[] morseCode = morse.toCharArray();
+        for (char a: morseCode){
+            if (a == '.'){
+
+            }
+        }
+    }
+}
---
+++ Node.java
@@ -0,0 +1,19 @@
+public class Node {
+    private char caracter;
+
+    private Node filhoEsquerdo;
+
+    private Node filhoDireito;
+
+    public char getCaracter() {
+        return caracter;
+    }
+
+    public void setFilhoEsquerdo(Node filhoEsquerdo) {
+        this.filhoEsquerdo = filhoEsquerdo;
+    }
+
+    public void setFilhoDireito(Node filhoDireito) {
+        this.filhoDireito = filhoDireito;
+    }
+}
